cd985dceac0a98af852429b5c586a4eff8887ab4
kevinchangjk <kevinchangjk@gmail.com>
2022-07-24 17:20:33 +0800

    modified configs for own usage

--- dalle/app.py
+++ dalle/app.py
@@ -7,13 +7,10 @@ import base64
 import os
 from pathlib import Path
 from io import BytesIO
-import time
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
-from consts import DEFAULT_IMG_OUTPUT_DIR
-from utils import parse_arg_boolean, parse_arg_dalle_version
-from consts import ModelSize
+from consts import Config
 
 app = Flask(__name__)
 CORS(app)
@@ -22,15 +19,12 @@ print("--> Starting DALL-E Server. This might take up to a minute.")
 from dalle_model import DalleModel
 dalle_model = None
 
-# these arguments just fix, don't let people change
-# save in consts.py
 parser = argparse.ArgumentParser(description = "A DALL-E API to generate images from a prompt")
 parser.add_argument("--port", type=int, default=8080, help = "backend port")
-parser.add_argument("--model_version", type = parse_arg_dalle_version, default = ModelSize.MINI, help = "Mini, Mega, or Mega_full")
-parser.add_argument("--save_to_disk", type = parse_arg_boolean, default = False, help = "Should save generated images to disk")
-parser.add_argument("--img_format", type = str.lower, default = "JPEG", help = "Generated images format", choices=['jpeg', 'png'])
-parser.add_argument("--output_dir", type = str, default = DEFAULT_IMG_OUTPUT_DIR, help = "Customer directory for generated images")
 args = parser.parse_args()
+
+model_size = Config.MODEL.value # enum to be used for DalleModel initialization
+image_format = Config.IMAGE_FORMAT.value.value
 
 @app.route("/dalle", methods=["POST"])
 @cross_origin()
@@ -41,23 +35,16 @@ def generate_images_api():
     generated_imgs = dalle_model.generate_images(text_prompt, num_images)
 
     returned_generated_images = []
-    if args.save_to_disk: 
-        dir_name = os.path.join(args.output_dir,f"{time.strftime('%Y-%m-%d_%H-%M-%S')}_{text_prompt}")
-        Path(dir_name).mkdir(parents=True, exist_ok=True)
-    
-    for idx, img in enumerate(generated_imgs):
-        if args.save_to_disk: 
-          img.save(os.path.join(dir_name, f'{idx}.{args.img_format}'), format=args.img_format)
-
+    for _, img in enumerate(generated_imgs):
         buffered = BytesIO()
-        img.save(buffered, format=args.img_format)
+        img.save(buffered, format=image_format)
         img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
         returned_generated_images.append(img_str)
 
     print(f"Created {num_images} images from text prompt [{text_prompt}]")
     
     response = {'generatedImgs': returned_generated_images,
-    'generatedImgsFormat': args.img_format}
+    'generatedImgsFormat': image_format}
     return jsonify(response)
 
 
@@ -68,11 +55,11 @@ def health_check():
 
 
 with app.app_context():
-    dalle_model = DalleModel(args.model_version)
+    dalle_model = DalleModel(model_size)
     # take out to minimise startup time
     # dalle_model.generate_images("warm-up", 1)
     print("--> DALL-E Server is up and running!")
-    print(f"--> Model selected - DALL-E {args.model_version}")
+    print(f"--> Model selected - DALL-E {model_size.value}")
 
 
 if __name__ == "__main__":
--- dalle/consts.py
+++ dalle/consts.py
@@ -1,7 +1,5 @@
 from enum import Enum 
 
-DEFAULT_IMG_OUTPUT_DIR = 'generations'
-
 DALLE_MODEL_MINI = "dalle-mini/dalle-mini/mini-1:v0"  # the original DALL-E Mini. Fastest yet suboptimal results
 DALLE_MODEL_MEGA = "dalle-mini/dalle-mini/mega-1-fp16:latest"  # the advanced version of DALL-E Mini. Requires more compute and VRAM
 DALLE_MODEL_MEGA_FULL = "dalle-mini/dalle-mini/mega-1:latest"  # DALL-E Mega. Warning: requires significantly more storage and GPU RAM
@@ -22,3 +20,11 @@ class ModelSize(Enum):
     MINI = "Mini"
     MEGA = "Mega"
     MEGA_FULL = "Mega_full"
+
+class ImageFormat(Enum):
+    JPEG = "jpeg"
+    PNG = "png"
+
+class Config(Enum):
+    MODEL = ModelSize.MINI
+    IMAGE_FORMAT = ImageFormat.PNG
