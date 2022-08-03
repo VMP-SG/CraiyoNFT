d01f6b5867be4d9bdff821582fd47a46039f3daf
kevinchangjk <kevinchangjk@gmail.com>
2022-08-03 10:12:21 +0800

    completed adding and reading test

--- backend/test.js
+++ backend/test.js
@@ -3,9 +3,16 @@ const path = require("path");
 async function loadIpfs() {
   const { create } = await import("ipfs");
   const jsipfsPath = path.join(__dirname, "jsipfs");
+  const API = "/ip4/127.0.0.1/tcp/5002";
+  const config = {
+    Addresses: {
+      API,
+    },
+  };
   const options = {
     repo: jsipfsPath,
     start: true,
+    config,
   };
 
   const node = await create(options);
@@ -14,12 +21,25 @@ async function loadIpfs() {
 }
 
 async function test() {
-  // Create the IPFS node instance
   const node = await loadIpfs();
-  // Your node is now ready to use \o/
+
+  const { globSource } = await import("ipfs");
+  const pathToFiles = path.join(__dirname, "test");
+  // const files = await node.addAll(globSource(pathToFiles, "**/*"));
+  // const file = await files.next();
+  // console.log(file);
+
+  for await (const file of node.addAll(globSource(pathToFiles, "**/*"))) {
+    console.log(file);
+  }
+
+  const cid = "QmSdNDti9QPLMCfQbjN6yyUg9xKyEY41baUzpBe8vBcCvn";
+
+  for await (const buf of node.get(cid)) {
+    console.log(buf);
+  }
 
   await node.stop();
-  // node is now 'offline'
 }
 
 test();
