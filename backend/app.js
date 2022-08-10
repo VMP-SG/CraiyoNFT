99aaf77bbb9827ff3a134b2ac637684250ab35b7
nghochi123 <nghochi123@gmail.com>
2022-08-10 23:50:33 +0800

    huix

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
+const Jimp = require("jimp");
+const axios = require("axios");
 
 let bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
-app.use(cors({origin: '*'}));
+app.use(cors({ origin: "*" }));
 
 const { Operator } = require("./operator");
 let operator;
@@ -19,19 +23,51 @@ Operator.init().then((result) => {
 app.post("/mintnft", (req, res) => {
   const prompt = req.body.prompt;
   console.log(`received prompt: ${prompt}`);
-  operator.mintNFT(prompt).then((log) => {
-    console.log(log);
-    res.send(log.toString());
-  }).catch((error) => res.send(error));
+  operator
+    .mintNFT(prompt)
+    .then((log) => {
+      console.log(log);
+      res.send(log.toString());
+    })
+    .catch((error) => res.send(error));
 });
 
 app.post("/getdata", (req, res) => {
   const cid = req.body.cid;
   console.log(`received cid: ${cid}`);
-  operator.getData(cid).then((log) => {
-    console.log(log);
-    res.send(log)
-  }).catch((error) => res.send(error));
+  operator
+    .getData(cid)
+    .then((log) => {
+      console.log(log);
+      res.send(log);
+    })
+    .catch((error) => res.send(error));
+});
+
+app.post("/getimage", async (req, res) => {
+  const backgroundarray = [
+    "abandoned_resized.jpg",
+    "shearing_resized.jpg",
+    "stars_resized.png",
+  ];
+  const selectedBGName =
+    backgroundarray[Math.floor(Math.random() * backgroundarray.length)];
+  const cid = req.body.cid;
+  const imagebuffer = await axios.post(
+    "https://craiyonft.nghochi.xyz/getdata",
+    {
+      cid: `${cid}`,
+    }
+  );
+  const images = imagebuffer.data.images;
+  const background = await Jimp.read(`./assets/${selectedBGName}`);
+  let position = 590;
+  for (let image of images) {
+    let img = await Jimp.read(Buffer.from(image, "base64"));
+    background.blit(img, position, 1000);
+    position += 590;
+  }
+  background.write("blit.png", res.download("blit.png"));
 });
 
 app.get("/", (req, res) => {
