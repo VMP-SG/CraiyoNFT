7cef431f4a50f46510e82de359262611a677cdfd
Chay Hui Xiang <chayhuixiang@gmail.com>
2022-08-12 23:31:57 +0800

    Added mail service

--- craiyonft-webapp/src/components/ContactCard.js
+++ craiyonft-webapp/src/components/ContactCard.js
@@ -1,17 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import PrimaryButton from "./PrimaryButton";
 import Github from "../assets/Github.svg";
 import Spinner from "./Spinner";
+import emailjs from '@emailjs/browser';
 
 const ContactCard = ({ image }) => {
   const [buttonContent, setButtonContent] = useState(<p>Contact Us</p>);
   const [sentMessage, setSentMessage] = useState(false);
-  const contactHandler = () => {
+  const form = useRef();
+  const contactHandler = (e) => {
+    e.preventDefault();
+    console.log(process.env);  
     setButtonContent(<Spinner variant="light" />);
-    setTimeout(() => {
-      setButtonContent(<p>Contact Us</p>);
-      setSentMessage(true);
-    },8000);
+    // setTimeout(() => {
+    //   setButtonContent(<p>Contact Us</p>);
+    //   setSentMessage(true);
+    // },8000);
+    emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, form.current, process.env.REACT_APP_PUBLIC_KEY)
+      .then((result) => {
+          console.log(result.text);
+          setButtonContent(<p>Contact Us</p>);
+          setSentMessage(true);
+      }, (error) => {
+          console.log(error.text);
+          setButtonContent(<p>Contact Us</p>);
+      });
+
   }
   return (
     <div
@@ -20,7 +34,7 @@ const ContactCard = ({ image }) => {
       }
     >
       <img className="w-full" src={image} alt="contact" />
-      <div className="px-6 py-4" onFocus={() => setSentMessage(false)}>
+      <form className="px-6 py-4" onFocus={() => setSentMessage(false)} onSubmit={contactHandler} ref={form}>
         <div className={`my-3`}>
           <label
             htmlFor="fullname"
@@ -31,6 +45,7 @@ const ContactCard = ({ image }) => {
           <input
             type="text"
             id="fullname"
+            name="fullname"
             className="block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
           />
         </div>
@@ -44,6 +59,7 @@ const ContactCard = ({ image }) => {
           <input
             type="text"
             id="email"
+            name="email"
             className="block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
           />
         </div>
@@ -57,14 +73,15 @@ const ContactCard = ({ image }) => {
           <input
             type="text"
             id="small-input"
+            name="message"
             className="block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
           />
         </div>
         <div className={`flex justify-between items-center`}>
           <div className="flex">
-            <PrimaryButton text={buttonContent} className="mr-[12.73px]" onClick={contactHandler}/>
+            <PrimaryButton text={buttonContent} className="mr-[12.73px]"/>
             <button
-              type="button"
+              type="submit"
               className="text-black font-medium rounded-lg text-sm p-1 text-center inline-flex items-center mr-2"
             >
               <a
@@ -78,7 +95,7 @@ const ContactCard = ({ image }) => {
           </div>
           {sentMessage && <p>Message Sent!</p>}
         </div>
-      </div>
+      </form>
     </div>
   );
 };
