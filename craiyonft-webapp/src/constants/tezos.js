566f1eb5bee5f6bb4a70c7af3d4092928addcc3a
Chay Hui Xiang <chayhuixiang@gmail.com>
2023-01-26 02:31:28 +0800

    Change backend address

--- craiyonft-webapp/src/constants/tezos.js
+++ craiyonft-webapp/src/constants/tezos.js
@@ -5,8 +5,9 @@ const RPC = {
 
 export const CONTRACTADDRESS = "KT1VDhUkMxipQvg6YmKaom2rikkbTjnWLAtU";
 // export const BACKENDADDRESS = "https://main-craiyo-nft-backend-chayhuixiang.endpoint.ainize.ai"
-export const BACKENDADDRESS = "https://craiyonft-backend-production.up.railway.app";
+// export const BACKENDADDRESS = "https://craiyonft-backend-production.up.railway.app";
 // export const BACKENDADDRESS = "https://1174-219-75-72-115.ap.ngrok.io";
+export const BACKENDADDRESS = "http://18.139.85.190:8080"
 
 export const localStorageKeys = {
     nftData: "craiyonft:nftdata"
